17c850f661811c028da7cc657a734af6f2632a82
Ayrton Vitor <ayrtonvitor@pm.me>
2022-10-24 13:48:31 -0300

    front page html now generated from template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -14,10 +14,10 @@ class NewVisitor(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element(By.TAG_NAME, 'h1')
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn('To-Do', header_text)
 
-        inputbox = self.browser.find_element(By.TAG_NAME, 'id_new_item')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -27,10 +27,11 @@ class NewVisitor(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element(By.ID, 'if_list_table')
-        rows = table.find_element_by(By.TAG_NAME, 'tr')
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_element(By.TAG_NAME, 'tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
         self.fail('Finish the test!')
 
